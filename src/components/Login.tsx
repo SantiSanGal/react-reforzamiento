5c9ec86f37f2d074e82eb2adec8311a61aa9d308
SantiSanGal <santiagon.g.m@gmail.com>
2023-08-26 21:59:36 -0400

    authReducer

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,34 @@
+import { useReducer } from "react"
+
+interface AuthState {
+    validando: boolean;
+    token: string | null;
+    username: string;
+    nombre: string;
+}
+
+const initialState: AuthState = {
+    validando: true,
+    token: null,
+    username: "",
+    nombre: ""
+}
+
+//objetos planos que no se pueden expandir
+type AuthAction = { type: "logout" };
+
+/*reducer debe retornar un estado del mismo tipo que initialState
+por eso definimos la interface y lo utilizamos en el objeto initialState
+y en la funcion reducer
+*/
+const authReducer = ( state: AuthState, action: AuthAction ): AuthState => {
+
+    
+}
+
 export const Login = () => {
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
   return (
     <>
         <h3>Login</h3>
